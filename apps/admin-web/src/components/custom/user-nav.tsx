888ec2f113846c05823a3a79c1ba2bc72773b203
George Padmore Yeboah <34770919+GeorgePadmore@users.noreply.github.com>
2024-08-16 16:49:05 +0000

    loging and logout improved

--- apps/admin-web/src/components/custom/user-nav.tsx
+++ apps/admin-web/src/components/custom/user-nav.tsx
@@ -10,8 +10,56 @@ import {
   DropdownMenuShortcut,
   DropdownMenuTrigger
 } from '@/components/ui/dropdown-menu';
+import Cookies from 'js-cookie'
+import { toast } from "react-toastify"
+import { useMutation } from "@tanstack/react-query"
+import { logout } from '@/api/data/query';
+import { useNavigate } from 'react-router-dom';
+
 
 export default function UserNav() {
+  const navigate = useNavigate();
+  const cookie = Cookies.get('user');
+  const user = cookie ? JSON.parse(cookie) : null;
+    
+
+  const logoutMutation = useMutation({
+    mutationFn: logout,
+  })
+
+
+  async function submitLogout () {
+    try {
+      
+      const res = await logoutMutation.mutateAsync();      
+      if (res?.response_code === "000") {
+
+        Cookies.remove('token');
+        Cookies.remove('user');
+        toast.success(res?.response_message);
+        navigate('/login')
+
+      } else {
+          toast.error(res?.response_message || "An error occurred during logout. Please try again.");
+      }
+
+    } catch (error) {
+      toast.error("An error occurred during logout. Please try again.");
+    }
+  }
+
+
+  async function settings () {
+    try {
+      
+      navigate('/settings')
+
+    } catch (error) {
+      toast.error("An error occurred during logout. Please try again.");
+    }
+  }
+
+
   return (
     <DropdownMenu >
       <DropdownMenuTrigger asChild>
@@ -30,32 +78,32 @@ export default function UserNav() {
       <DropdownMenuContent className="w-56 bg-white" align="end" forceMount>
         <DropdownMenuLabel className="font-normal">
           <div className="flex flex-col space-y-1">
-            <p className="text-sm font-medium leading-none">{'Admin'}</p>
+            <p className="text-sm font-medium leading-none">{user ? `${user?.first_name} ${user?.last_name}` : 'User Avatar'}</p>
             <p className="text-xs leading-none text-muted-foreground">
-              {'admin@gmail.com'}
+              {user ? `${user?.email}` : 'Email'}
             </p>
           </div>
         </DropdownMenuLabel>
         <DropdownMenuSeparator />
         <DropdownMenuGroup>
-          <DropdownMenuItem>
+          {/* <DropdownMenuItem>
             Profile
             <DropdownMenuShortcut>⇧⌘P</DropdownMenuShortcut>
-          </DropdownMenuItem>
-          <DropdownMenuItem>
+          </DropdownMenuItem> */}
+          {/* <DropdownMenuItem>
             Billing
             <DropdownMenuShortcut>⌘B</DropdownMenuShortcut>
-          </DropdownMenuItem>
-          <DropdownMenuItem>
+          </DropdownMenuItem> */}
+          <DropdownMenuItem onClick={() => settings()}>
             Settings
-            <DropdownMenuShortcut>⌘S</DropdownMenuShortcut>
+            <DropdownMenuShortcut></DropdownMenuShortcut>
           </DropdownMenuItem>
-          <DropdownMenuItem>New Team</DropdownMenuItem>
+          {/* <DropdownMenuItem>New Team</DropdownMenuItem> */}
         </DropdownMenuGroup>
         <DropdownMenuSeparator />
-        <DropdownMenuItem onClick={() => console.log('logout')}>
+        <DropdownMenuItem onClick={() => submitLogout()}>
           Log out
-          <DropdownMenuShortcut>⇧⌘Q</DropdownMenuShortcut>
+          <DropdownMenuShortcut></DropdownMenuShortcut>
         </DropdownMenuItem>
       </DropdownMenuContent>
     </DropdownMenu>
